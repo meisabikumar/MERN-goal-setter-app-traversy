2e271668c176b69f15e33c2652c3b6a063684b79
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-04-06 00:32:58 +0530

    create API endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,4 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => res.send("Hello World!"));
