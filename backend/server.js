def9fbe64f6244f072f94405748b96e775d19a51
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-04-06 00:30:06 +0530

    set up .env file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const dotenv = require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
