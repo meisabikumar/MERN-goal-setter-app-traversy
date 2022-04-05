35b439b6c6c0229f4a13e1e0b13ae2e8ba2e8c45
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-04-06 01:05:13 +0530

    cleanup routes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { getGoals, setGoal, updateGoals, deleteGoals } = require("../controllers/goalControllers");
 
-router.get("/", getGoals);
+router.route("/").get(getGoals).post(setGoal);
+// router.get("/", getGoals);
+// router.post("/", setGoal);
 
-router.post("/", setGoal);
-
-router.put("/:id", updateGoals);
-
-router.delete("/:id", deleteGoals);
+router.route("/:id").put(updateGoals).delete(deleteGoals);
+// router.put("/:id", updateGoals);
+// router.delete("/:id", deleteGoals);
 
 module.exports = router;
