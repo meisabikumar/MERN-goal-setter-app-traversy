7e4bbdfc6a5563af5651a8c290c54574a1cc3382
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-04-07 00:16:50 +0530

    npm i express-async-handler
    async handler

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,26 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
 // @route  GET /api/goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goal !" });
-};
+});
 
 // @route  POST /api/goals
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("something went wrong");
   }
   res.status(200).json(req.body);
-};
+});
 
 // @route  PUT /api/goals/:id
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @route  DELETE /api/goals/:id
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
