a3eab8f7619287835639441d8e9667745ce5e64c
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-06-15 22:03:49 +0530

    for production build

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 
-connectDB()
+connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"));
+  });
+}else{
+    app.get('/', (req, res) => {
+        res.send('Please set to Production!')
+    })
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
